8aecf754acd49a788f14c2108e89c8fc7024770b
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-04-06 17:28:10 +0200

    Update yeet2.glsl

--- yeet2.glsl
+++ yeet2.glsl
@@ -1,11 +1,17 @@
+
+
+
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-      vec3 rect = vec3(0.5,0.5,0.5);
-      float xcoord = fragCoord[0]/iResolution[0];
+      vec4 rect = vec4(0.5,(cos(iTime)+ 1.5) / 3.0,1.2,(cos(iTime + 1.0)+ 1.5) / 3.0);
+      float xcoord = fragCoord[0]/iResolution[1];
       float ycoord = fragCoord[1]/iResolution[1];
       float afstandx = abs(rect[0] - xcoord);
       float afstandy = abs(rect[1] - ycoord);
-      afstandx -= abs(afstandx - (1.0 / iTime));
-      afstandy -= abs(afstandy - (1.0 / iTime));
-      fragColor[0] = 1.0 - sqrt(afstandx * afstandx + afstandy * afstandy) * iTime;
+      float afstandx2 = abs(rect[2] - xcoord);
+      float afstandy2 = abs(rect[3] - ycoord);
+      float yeet = 1.0 - sqrt(afstandx * afstandx + afstandy * afstandy) * 6.0;
+      float yeet2 = 1.0 - sqrt(afstandx2 * afstandx2 + afstandy2 * afstandy2) * 6.0;
+      fragColor[0] = pow(yeet,sqrt(afstandx * afstandx + afstandy * afstandy));
+      fragColor[1] = pow(yeet2,sqrt(afstandx2 * afstandx2 + afstandy2 * afstandy2));
 }
