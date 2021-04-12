1ac88f1e6c1bd3c29bea1d2c4ad860f58f79dff9
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-04-12 12:56:27 +0200

    Update yeet4.glsl

--- yeet4.glsl
+++ yeet4.glsl
@@ -3,14 +3,13 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
    float xcoord = fragCoord[0] / iResolution[0];
    float ycoord = fragCoord[1] / iResolution[1];
-   vec2 rays = vec2(1.0,1.0);
-   for (float i = 0.0;i < 1.0;i++){
-       vec4 col = texture(iChannel0,vec2(rays[0],rays[1]));
-       if (col[0] < 0.5){
-       rays[0] -= xcoord;
-       rays[1] -= ycoord;
-       }
+   vec4 col = texture(iChannel0,vec2(xcoord,ycoord));
+   vec4 col2 = texture(iChannel0,vec2(fract(xcoord + 0.03),ycoord));
+   vec2 rays = vec2(0.0,0.0);
+   for (float i = 0.0;i < 2.0;i++){
+       col[0] -= col[0] - col2[0];
+       col[1] -= col[2] - col2[1];
+       col[2] -= col[1] - col2[2];
    }
-   vec4 col = texture(iChannel0,vec2(rays[0],rays[1]));
    fragColor = col;
 }
