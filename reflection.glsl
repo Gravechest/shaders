3b3493194a851122ae15d68347527dd11c1b0893
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-04-26 00:08:51 +0200

    Update reflection.glsl

--- reflection.glsl
+++ reflection.glsl
@@ -1,28 +1,34 @@
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
+    float afstand = 1.0;
     float xcoord = fragCoord[0] / iResolution[0];
     float ycoord = fragCoord[1] / iResolution[1];
-    float sphere[6] = float[6](1.0,1.0,20.0,4.0,1.0,20.0);
-    vec3 ray = vec3(-1.0,-1.0,0.0);
-    for (int i = 0;i < 10;i += 1){
-        float rayjump = 1000.0;
-        for (int i2 = 0;i2 < sphere.length();i2+=3){
-            float xAxis = sphere[i2] - ray[0];
-            float yAxis = sphere[i2 + 1] - ray[1];
-            float zAxis = sphere[i2 + 2] - ray[2];
-            rayjump = min(sqrt(xAxis * xAxis + yAxis * yAxis + zAxis * zAxis),rayjump);
+    vec3 ray = vec3(0.0,0.0,0.0);
+    float sphere[3] = float[3](1.5,1.5,iTime / 4.0);
+    for (int i = 0;i < 100;i++){
+        ray[0] += xcoord / 10.0;
+        ray[1] += ycoord / 10.0;
+        ray[2] += afstand / 10.0;
+        float xAxis = abs(ray[0] - sphere[0]);
+        float yAxis = abs(ray[1] - sphere[1]);
+        float zAxis = abs(ray[2] - sphere[2]);
+        if (xAxis * xAxis + yAxis * yAxis + zAxis * zAxis < 0.2){
+            fragColor[0] = fract(zAxis);
+            break;
         }
-        ray[0] += xcoord * rayjump;
-        ray[1] += ycoord * rayjump; 
-        ray[2] += rayjump;
-        if (rayjump < 10.0){
+        if (ray[1] > 4.0){
+            fragColor[1] = 1.0 - float(i) / 100.0;
             break;
         }
     }
-    for (int i = 0;i < sphere.length();i+=3){
-        float xAxis = sphere[i] - ray[0];
-        float yAxis = sphere[i + 1] - ray[1];
-        float zAxis = sphere[i + 2] - ray[2];
-        fragColor[0] += 3.0 - sqrt(xAxis * xAxis + yAxis * yAxis + zAxis * zAxis);
+    float xAxis = abs(ray[0] - sphere[0]);
+    float yAxis = abs(ray[1] - sphere[1]);
+    float zAxis = abs(ray[2] - sphere[2]);
+    if (ray[1] > 4.0){
+        fragColor[2] = 1.0;
     }
+    xAxis = ray[0] - sphere[0];
+    yAxis = ray[1] - sphere[1];
+    zAxis = ray[2] - sphere[2];
+    
 }
