dbaaef5559b6719acd06591e1492dbe98b986b2e
Gravechest <75589056+Gravechest@users.noreply.github.com>
2021-04-27 17:45:47 +0200

    Update reflection.glsl

--- reflection.glsl
+++ reflection.glsl
@@ -4,26 +4,29 @@ float distant(vec3 point,vec3 dest){
 }
 void mainImage( out vec4 fragColor, in vec2 fragCoord )
 {
-    float xcoord = fragCoord[0]/iResolution[0] * 1.3;
+    float bounce = 0.0;
+    float xcoord = fragCoord[0]/iResolution[0];
     float ycoord = fragCoord[1]/iResolution[1];
     float trvl = 1.0;
-    float sphere[8] = float[8](1.5,2.5,5.0,1.0,5.5,iTime,6.0,1.0);
+    xcoord -= (iTime + xcoord);
+    float sphere[8] = float[8](1.5,2.5,5.0,1.0,3.5,2.0,5.0,1.0);
     vec3 source = vec3(0.0,0.0,0.0);
     vec3 light = vec3(10.0,10.0,10.0);
-    vec3 ray = vec3(0.0,0.0,0.0);
+    vec3 ray = vec3(iTime,0.0,0.0);
     for(int i = 0;i < 100;i++){
-        float dist = distant(vec3(sphere[0],sphere[1],sphere[2]),ray);
+        float dist = distant(vec3(sphere[0],sphere[1],sphere[2]),ray) - sphere[3];
         for(int i2 = 0;i2 < sphere.length();i2+=4){
             float distvb = distant(vec3(sphere[i2],sphere[i2+1],sphere[i2+2]),ray);
-            dist = min(dist,distvb);
+            dist = min(dist,distvb - sphere[i2+3]);
         }
-        dist -= 1.0;
         if (dist < 0.001){
             vec3 reflectie = reflect(vec3(ray[0] - sphere[0],ray[1] - sphere[1]
             ,sphere[2] - ray[2]),vec3(source));
             xcoord = reflectie[0];
             ycoord = reflectie[1];
             trvl = reflectie[2] - 2.0;
+            bounce += 1.0;
+            dist = 0.1;
         }
         ray[0] += xcoord * dist;
         ray[1] += ycoord * dist;
@@ -32,10 +35,11 @@ void mainImage( out vec4 fragColor, in vec2 fragCoord )
             break;
         }
     }
-    if (ray[0] < 10.0){
-    fragColor[0] = ray[2] / 2.0;
+    if (bounce == 1.0){
+        fragColor[0] = 1.0;
     }
-    if (ray[0] > 1.0){
-        fragColor[1] = ray[1] / 60.0;
+    if (bounce >= 2.0){
+        fragColor[1] = 1.0;
     }
+
 } 
